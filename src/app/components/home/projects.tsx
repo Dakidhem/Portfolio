2d7ccdc2b324863fd0b0a44b648ef71d57451604
[MehdiSfizer] <[mehdi@sfizer.consulting]>
2023-07-13 17:06:43 +0100

    Added projects

--- src/app/components/home/projects.tsx
+++ src/app/components/home/projects.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
       </h3>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 py-8 px-4">
         {projects_info.map((project) => {
-          return <Project project={project} />;
+          return <Project key={project.project_id} project={project} />;
         })}
       </div>
     </section>
