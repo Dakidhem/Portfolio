bd33f21085651031fd77f94c42bf99b8f9d24fac
[MehdiSfizer] <[mehdi@sfizer.consulting]>
2023-08-15 09:00:40 +0100

    Fixing projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Projects from "./components/projects/projects";
 import HomeSectionSeparator from "@/app/components/utils/HomeSectionSeparator";
 import Hero from "@/app/components/hero/hero";
 import Experiance from "@/app/components/experiance/experiance";
-import About from "@/app/components/about/about";
 import Techs from "./components/techs/techs";
 import { motion } from "framer-motion";
 import RightBar from "./components/shared/rightBar";
